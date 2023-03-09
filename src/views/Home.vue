653a1e05c9ccb3da61ac91bb26aa35ba610b71f3
handrihmw <handrihmw@gmail.com>
2023-03-09 21:24:45 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container mx-auto px-4">
-    <h1 class="mb-4 text-3xl font-bold text-gray-800 dark:text-white">
-      Card Components
-    </h1>
-    <p class="font-light text-gray-500 dark:text-gray-400">
-      Here is an example of using a card component using the Vue js 3 Composition API, Typescript, and Tailwind CSS
-    </p>
+    <div class="flex justify-between items-center mb-3">
+      <div>
+        <h1 class="mb-4 text-3xl font-bold text-gray-800 dark:text-white">
+          Card Components
+        </h1>
+        <p class="font-light text-gray-500 dark:text-gray-400">
+          Here is an example of using a card component using the Vue js 3 Composition API, Typescript, and Tailwind CSS
+        </p>
+      </div>
+      <TemplateDark />
+    </div>
+    
     <hr class="h-px mt-5 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
     <div>
       <TemplateCard />
@@ -14,5 +20,6 @@
 </template>
 
 <script setup lang="ts">
+import TemplateDark from '@/components/templates/TemDark.vue'
 import TemplateCard from '@/components/templates/TemCard.vue'
 </script>
